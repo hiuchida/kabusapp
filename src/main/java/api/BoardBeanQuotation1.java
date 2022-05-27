6ac57d7bb77b915b5e4a8503b2f2238277f03fb8
hiuchida <hiro4uchida@gmail.com>
2022-05-27 14:22:28 +0900

    株式のみ気配のOffsetDateTime型をString型に変更する。

--- src/main/java/api/BoardBeanQuotation1.java
+++ src/main/java/api/BoardBeanQuotation1.java
@@ -1,18 +1,16 @@
 package api;
 
-import org.threeten.bp.OffsetDateTime;
-
 import com.google.gson.annotations.SerializedName;
 
 /**
- * PUSH配信された時価情報の気配数量1本目。
+ * PUSH配信された時価情報の気配数量1本目。OffsetDateTime型はString型に変更。
  * 
  * @see io.swagger.client.model.BoardSuccessSell1
  * @see io.swagger.client.model.BoardSuccessBuy1
  */
 public class BoardBeanQuotation1 {
 	@SerializedName("Time")
-	public OffsetDateTime time = null;
+	public String /* OffsetDateTime */ time = null;
 
 	@SerializedName("Sign")
 	public String sign = null;
